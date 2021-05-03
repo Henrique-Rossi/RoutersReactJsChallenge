fa5a1afb3e1a62e5716935d4ad8f65c7f79a5458
Henrique-Rossi <62766483+Henrique-Rossi@users.noreply.github.com>
2021-05-03 20:17:39 -0300

    commit

--- src/components/FilterBar/index.jsx
+++ src/components/FilterBar/index.jsx
@@ -1,8 +1,13 @@
 import { DropdownButton, Dropdown, Button } from 'react-bootstrap'
 import './style.css'
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { useState } from 'react';
+
 
 export const FilterBar = () => {
-      
+    const [startDate, setStartDate] = useState(new Date());
+  
     return (
         <div className="center-filter">
             <div className="filterbar">
@@ -28,33 +33,28 @@ export const FilterBar = () => {
                 <div className="Check-in">
                     <h5>Check-in</h5>
                     
-                    <input type="numeric"></input>
+                    <DatePicker selected={startDate} onChange={date => setStartDate(date)} />
 
                 </div>
                 <div className="Check-out">
                     <h5>Check-out</h5>
-                    <DropdownButton id="dropdown-item-button" title="Boa Esperança">
-                        
-                        <Dropdown.Item as="button">Belo Horizonte</Dropdown.Item>
-                        <Dropdown.Item as="button">Varginha</Dropdown.Item>
-                        <Dropdown.Item as="button">Três Corações</Dropdown.Item>
-                    </DropdownButton>
+                    <DatePicker selected={startDate} onChange={date => setStartDate(date)} />
 
                 </div>
 
                 <div className="hospedes">
-                    <h5>hospedes</h5>
-                    <DropdownButton id="dropdown-item-button" title="Boa Esperança">
+                    <h5>Hóspedes</h5>
+                    <DropdownButton id="dropdown-item-button" title="1 Hóspedes">
                        
-                        <Dropdown.Item as="button">Belo Horizonte</Dropdown.Item>
-                        <Dropdown.Item as="button">Varginha</Dropdown.Item>
-                        <Dropdown.Item as="button">Três Corações</Dropdown.Item>
+                        <Dropdown.Item as="button">1 Hóspedes</Dropdown.Item>
+                        <Dropdown.Item as="button">2 Hóspedes</Dropdown.Item>
+                        <Dropdown.Item as="button">3  Hóspedes</Dropdown.Item>
                     </DropdownButton>
 
                 </div>
 
                 <div className="busca">
-                <Button variant="danger">Busca</Button>
+                <Button >Busca</Button>
                 </div>
                 <div className="filtersplus">
                     <a href="/#"><h6>+ mais filtros</h6></a>
